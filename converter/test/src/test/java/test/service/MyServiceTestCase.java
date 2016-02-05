5565f9c412a683044fb28c45b8597312da498a2b
Saiyantan Ghosh <saiyantan.ghosh@gmail.com>
2016-02-05 18:29:43 +0530

    osgi project structure

--- converter/test/src/test/java/test/service/MyServiceTestCase.java
+++ converter/test/src/test/java/test/service/MyServiceTestCase.java
@@ -1,34 +1,36 @@
 package test.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+
+import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
-import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 
 @RunWith(PaxExam.class)
-@ExamReactorStrategy(PerMethod.class)
 public class MyServiceTestCase {
 	@Inject
 	private BundleContext ctx;
-	public static Option config() {
+	
+	@Configuration
+	public static Option[] config() {
 	 
-	        return combine(
-	            mavenBundle("com.converter.api", "api", "1.0.0-SNAPSHOT"),
-	            junitBundles()
+	        return CoreOptions.options(
+	        		CoreOptions.mavenBundle("com.converter.api", "api", "1.0-SNAPSHOT"),
+	        		CoreOptions.junitBundles()
 	            );
 	    }
 	 
 	    @Test
 	    public void getHelloService() {
-	    	ServiceReference ref = ctx.getServiceReference(MyService.class.getName());
-	        MyService svc = (MyService) ctx.getService(ref);
-	        assertEquals("Hello Pax!", helloService.getMessage());
+	    	ServiceReference ref = ctx.getServiceReference(ExampleService.class.getName());
+	    	ExampleService svc = (ExampleService) ctx.getService(ref);
+	        assertEquals("Hello Pax!", svc.getMessage());
 	    }
 }
